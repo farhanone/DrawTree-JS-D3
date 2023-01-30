908065d42a0d9444f83ea9c5e7267cca73d99d3d
Farhan Rasheed <farhan.rasheed@liu.se>
2023-01-30 11:48:51 +0100

    Add color to nodes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the diagram
 var margin = { top: 30, right: 90, bottom: 30, left: 90 },
     width = 1280   - margin.left - margin.right,
-    height = 1024  - margin.top - margin.bottom;
+    height = 1020  - margin.top - margin.bottom;
     scale_margin = 0;
 
 
@@ -20,10 +20,15 @@ function range(start, end, step = 1) {
     return Array(len).fill().map((_, idx) => start + (idx * step))
     }
 var scaleticks = range(0, height, height/10);
-console.log()
+
 // scaleticks.reverse()
+// var temp = d3.scaleLinear()
+//     .domain([0,1])
+//     .range([0, 10])
+//     // cc = spectral[temp(colors(2))]
 
-
+// var spectral = colorbrewer.Spectral[11]
+// console.log(temp(0))
 
 // console.log(colorbrewer.YlGnBu[9])
 
@@ -58,17 +63,23 @@ d3.json("treeData.json").then(function(data){
         .domain([height, 0])
         .range([0, max_val])
 
-        console.log(canvas2f(max_val))
-        console.log(f2canvas(canvas2f(scaleticks[0])))
-        console.log(max_val)
-    // temp_C = d3.scaleLinear()
-    //     .domain([0, max_val])
-    //     .range([0, 10])
+    var n_color = 11
+    var total_leaves = root.sum(function(d) { return d.value ? 1 : 0; });
+    // var total_leaves = root.descendants().count()
+    console.log(total_leaves.value)
+    d_colormap = d3.schemeSpectral[11]
+    // d_colormap =colorbrewer.[n_color]
+    cid2cs = d3.scaleLinear()
+        .domain([0, total_leaves.value])
+        .range([0, n_color-1]);
 
-    // colors = d3.scaleQuantize()
-    // .domain([0, max_val])
-    // .range(d3.interpolate(colorbrewer.Spectral[10]))
 
+    var colors = function(cid) {var abs_ = cid2cs(cid), c_1= Math.floor(abs_), t= abs_- Math.floor(abs_)
+        console.log(d_colormap[c_1], d_colormap[c_1 + 1],abs_)
+        return d3.interpolate(d_colormap[c_1], d_colormap[c_1 + 1])(t);
+        }
+        // var abs_ = cid2cs(81)
+        
 
     tree(root);
 
@@ -93,6 +104,51 @@ d3.json("treeData.json").then(function(data){
             return diagonal.toString();
         });
 
+
+
+
+        
+        var Tooltip  = d3.select("#tooltip").append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("position", "absolute")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "2px")
+        .style("border-radius", "5px")
+        .style("padding", "5px")
+        // .style("visibility", "hidden")
+        // .style("font-size", "20px")
+        // .style('font-family', 'Helvetica')
+        // .style("background-color", "white")
+        // .style("border", "solid")
+        // .style("border-width", "2px")
+        // .style("border-radius", "5px")
+        // .style("padding", "5px")
+        // .style("stroke", "black")
+
+        var mouseover = function(d) {
+            Tooltip
+              .style("opacity", 1)
+            d3.select(this)
+              .style("stroke", "black")
+              .style("opacity", )
+          }
+          var mousemove = function(e,d) {
+            Tooltip
+              .html("CycleId: " + d.data.name +"<br/>" + "Force: "
+                + d.data.value)
+              .style("left", (e.pageX +10) + "px")
+              .style("top", (e.pageY) + "px")              
+          }
+          var mouseleave = function(d) {
+            Tooltip
+              .style("opacity", 0)
+            d3.select(this)
+              .style("stroke", "none")
+              .style("opacity", 1)
+          }
+
     // // create the node elements
     var node = svg.selectAll(".node")
     .data(root.descendants())
@@ -101,45 +157,20 @@ d3.json("treeData.json").then(function(data){
         .attr("transform", function(d) { return "translate(" + d.x + "," + f2canvas(invert_f(d.data.value)) + ")"; });
 
         // add the circle to the node
-        node.append("circle")
-        .attr("r", 5);
-        
+        svg.selectAll(".node--internal").append("circle")
+        .attr("r", 5)
+        .style("fill", function(d){ 
+            return colors(d.data.name)})
+        svg.selectAll(".node--leaf").append("circle")
+        .attr("r", 3)
+        .style("fill", function(d){ 
+            return colors(d.data.name)})
+
+        svg.selectAll(".node").on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave);
 
         
-        tooltip = svg.append("g")
-        .attr("class","hover-text")
-        .attr("dy", "-1em")
-        .append("text")
-        .style("visibility", "hidden")
-        .style("font-size", "20px")
-        .style('font-family', 'Helvetica')
-        // .style("background-color", "white")
-        // .style("border", "solid")
-        // .style("border-width", "2px")
-        // .style("border-radius", "5px")
-        // .style("padding", "5px")
-        // .style("stroke", "black")
-        // .attr("cx", 100)
-
-        console.log(node)
-
-        node.on("mouseover", (elem, d)=> {
-            d3.select(".hover-text").select("text")
-                // // .attr("id", "tooltip")
-            .attr("x", d.x)
-            .attr("y", f2canvas(invert_f(d.data.value)))
-
-            .style("visibility", "visible")
-            .text(d.data.value.toFixed(4));
-        })
-        .on("mouseout", (elem, d)=> {
-            // d3.select("#tooltip").remove();
-            d3.select(".hover-text").select("text")
-                .style("visibility", "hidden");
-
-        });
-        
-    
 
     // add scale bar
     var scale = svg.selectAll(".scale")
