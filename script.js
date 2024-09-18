c7575bf4ca541373bc694657a115bf5e1b94e0ed
Farhan Rasheed <farhan.rasheed@liu.se>
2024-09-18 11:59:31 +0200

    update CSS, add dragable line

--- script.js
+++ script.js
@@ -21,135 +21,138 @@ var tree = d3.cluster()
     .separation((a, b) => a.parent === b.parent ? 1 : 2);
 
 
-d3.json("treeData.json").then(function(data){
+d3.json("treeData.json").then(function (data) {
 
     // compute the layout of the nodes based on the data
     root = d3.hierarchy(data);
     console.log(root)
     var values = [];
 
-    root.each(function(d) {values.push(d.data.value);});
+    root.each(function (d) { values.push(d.data.value); });
 
 
-    heightScale = d3.scaleLinear()
+    data2Canvas = d3.scaleLinear()
         .domain([0, root.data.value])  // Min value to max value
         .range([height - margin.top - margin.bottom, 0]);
+
+    canvas2Data = d3.scaleLinear()
+        .domain([0, height - margin.top - margin.bottom])
+        .range([root.data.value, 0]);  // Min value to max value
         
+
     tree(root);
 
     var diagonal = d3.path();
     // create the link elements
-    
+
     var link = svg.selectAll(".link")
         .data(root.descendants().slice(1))
         .enter().append("path")
-        .style("stroke-width", 1)
         .attr("class", "link")
-        .style("stroke", function (d) {
-            return "#535353"})
         .attr("d", function (d, i) {
-            diagonal.moveTo(d.parent.x, heightScale(d.parent.data.value));
-            diagonal.lineTo(d.x, heightScale(d.parent.data.value));
-            diagonal.lineTo(d.x, heightScale(d.data.value));
+            diagonal.moveTo(d.parent.x, data2Canvas(d.parent.data.value));
+            diagonal.lineTo(d.x, data2Canvas(d.parent.data.value));
+            diagonal.lineTo(d.x, data2Canvas(d.data.value));
             return diagonal.toString();
         });
 
-        
-        var Tooltip  = d3.select("#tooltip").append("div")
-        .style("opacity", 0)
-        .attr("class", "tooltip")
-        .style("position", "absolute")
-        .style("background-color", "white")
-        .style("border", "solid")
-        .style("border-width", "2px")
-        .style("border-radius", "5px")
-        .style("padding", "5px")
-        // .style("visibility", "hidden")
-        // .style("font-size", "20px")
-        // .style('font-family', 'Helvetica')
-        // .style("background-color", "white")
-        // .style("border", "solid")
-        // .style("border-width", "2px")
-        // .style("border-radius", "5px")
-        // .style("padding", "5px")
-        // .style("stroke", "black")
 
-        var mouseover = function(d) {
-            Tooltip
-              .style("opacity", 1)
-            d3.select(this)
-              .style("stroke", "black")
-              .style("opacity", )
-          }
-          var mousemove = function(e,d) {
-            Tooltip
-              .html("CycleId: " + d.data.name +"<br/>" + "Force: "
+    var Tooltip = d3.select("#tooltip").append("div")
+        .attr("class", "tooltip")
+
+
+    var mouseover = function (d) {
+        Tooltip
+            .style("opacity", 1)
+        d3.select(this)
+            .style("stroke", "black")
+            .style("opacity",)
+    }
+    var mousemove = function (e, d) {
+        Tooltip
+            .html("CycleId: " + d.data.name + "<br/>" + "Force: "
                 + d.data.value)
-              .style("left", (e.pageX +10) + "px")
-              .style("top", (e.pageY) + "px")              
-          }
-          var mouseleave = function(d) {
-            Tooltip
-              .style("opacity", 0)
-            d3.select(this)
-              .style("stroke", "none")
-              .style("opacity", 1)
-          }
+            .style("left", (e.pageX + 10) + "px")
+            .style("top", (e.pageY) + "px")
+    }
+    var mouseleave = function (d) {
+        Tooltip
+            .style("opacity", 0)
+        d3.select(this)
+            .style("stroke", "none")
+            .style("opacity", 1)
+    }
 
     // // create the node elements
     var node = svg.selectAll(".node")
-    .data(root.descendants())
-    .enter().append("g")
-        .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
-        .attr("transform", function(d) { return "translate(" + d.x + "," + heightScale(d.data.value) + ")"; });
+        .data(root.descendants())
+        .enter().append("g")
+        .attr("class", function (d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
+        .attr("transform", function (d) { return "translate(" + d.x + "," + data2Canvas(d.data.value) + ")"; });
 
-        // add the circle to the node
-        svg.selectAll(".node--internal").append("circle")
-        .attr("r", 5)
-        .style("fill", function(d){ 
-            return "#A52A2Acc"})
-        svg.selectAll(".node--leaf").append("circle")
-        .attr("r", 4)
-        .style("fill", function(d){ 
-            return "#A52A2Acc"})
+    // add the circle to the node
+    svg.selectAll(".node--internal").append("circle")
+        .attr("r", 5);
+    svg.selectAll(".node--leaf").append("circle")
+        .attr("r", 4);
 
-        svg.selectAll(".node").on("mouseover", mouseover)
+    svg.selectAll(".node").on("mouseover", mouseover)
         .on("mousemove", mousemove)
         .on("mouseleave", mouseleave);
 
 
-        // Add a vertical scale on the right side
-        const scaleContainer = svg.append("g")
-            .attr("transform", `translate(${width - margin.right + 10}, 0)`);  // Position scale on the right
+    // Add a vertical scale on the right side
+    const scaleContainer = svg.append("g")
+        .attr("transform", `translate(${width - margin.right + 10}, 0)`);  // Position scale on the right
 
-        // Draw the scale line
-        scaleContainer.append("line")
-            .attr("class", "scale-line")
-            .attr("x1", 0)
-            .attr("x2", 0)
-            .attr("y1", 0)
-            .attr("y2", height - margin.top - margin.bottom);
+    // Draw the scale line
+    scaleContainer.append("line")
+        .attr("class", "scale-line")
+        .attr("x1", 0)
+        .attr("x2", 0)
+        .attr("y1", 0)
+        .attr("y2", height - margin.top - margin.bottom);
 
-        const scaleAxis = d3.axisRight(heightScale)
-            .ticks(6)  // Number of ticks on the axis
-            .tickSize(5);
+    const scaleAxis = d3.axisRight(data2Canvas)
+        .ticks(6)  // Number of ticks on the axis
+        .tickSize(5);
 
-        // Append the scale axis
-        scaleContainer.call(scaleAxis);
+    // Append the scale axis
+    scaleContainer.call(scaleAxis);
+
+    const threshold = document.getElementById("InputThreshold").value;
+
+    // add horizontal cut line
+    var cutLine = d3.select("svg")
+    .append("line")
+    .attr("id", "cut-threshold")
+    .attr("x1", margin.left)
+    .attr("y1", data2Canvas(threshold) + margin.top)
+    .attr("x2", width)
+    .attr("y2", data2Canvas(threshold) + margin.top)
+    .style("stroke", "red")
+    .style("stroke-width", "2")
+    .style("cursor", "move")
+    .call(drag);  // Add drag functionality if required
+
+});
+
+// Callback functions
+
+function thresholdChanged() {
+    var threshold = document.getElementById("InputThreshold").value;
+    var line = document.getElementById("cut-threshold");
+    var y = data2Canvas(threshold) + margin.top
+    line.setAttribute("y1", y)
+    line.setAttribute("y2", y)
+}
+
+var drag = d3.drag()
+    .on("drag", function (event) {
+        var newY = Math.max(margin.top, Math.min(height - margin.bottom, event.y));
+        d3.select(this)
+            .attr("y1", newY)
+            .attr("y2", newY);
+        document.getElementById("InputThreshold").value = canvas2Data(newY-margin.top)
         
-        threshold = 0.05
-        var line = d3.select("svg")
-            .append("line")
-            .attr("id", "threshold")
-            .attr("x1", 0)
-            .attr("y1", heightScale(threshold) + margin.top)
-            .attr("x2", width)
-            .attr("y2", heightScale(threshold) + margin.top)
-            .style("stroke", "red")      // Set the color of the line
-            .style("stroke-width", "2");
-            // .style("cursor", "move");
-            // .call(drag);
-
-
-
-        });
+    });
